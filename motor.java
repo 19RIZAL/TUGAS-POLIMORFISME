7688c809d13f01b5939efc21dffed3b639ad9bb0
19RIZAL <114996147+19RIZAL@users.noreply.github.com>
2022-10-21 21:01:06 +0700

    Add files via upload

---
+++ motor.java
@@ -0,0 +1,31 @@
+class motor{
+    private int harga;
+    private int TotalBayar;
+
+  
+    public motor(int harga, int jumlah){
+        this.harga = harga;
+        this.TotalBayar = harga * jumlah;
+
+    }
+
+
+    public double getTotalBayar(){
+        return TotalBayar;
+    }
+}
+
+public class Main {
+     public static void main (String[] args){
+        motor beat = new motor(17000000, 1);
+        motor vario = new motor(34000000, 2);
+        motor aerox = new motor(25000000, 1);
+        motor fino  = new motor(17000000, 1);
+
+        System.out.println("Perhitungan harga motor");
+        System.out.println("harga motor beat : " + beat.getTotalBayar());
+        System.out.println("harga motor vario : " + vario.getTotalBayar());
+        System.out.println("harga motor aeorx : " +aerox.getTotalBayar());
+        System.out.println("harga motor fino : " + fino.getTotalBayar());
+    }
+}
